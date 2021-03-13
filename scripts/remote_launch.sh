9979af0b18a02518228e83b8eed326ad708762f4
kellencataldo <kellencataldo@gmail.com>
2021-03-13 12:45:04 -0500

    migrated rulebot to its own folder in aws

--- scripts/remote_launch.sh
+++ scripts/remote_launch.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+set -e
+
 go build ../cmd/rulebot.go
 retVal=$?
 if [ $retVal -ne 0 ]; then
     exit
 fi
 
-ssh -i $SECRETS/aws-rulebot.pem -o StrictHostKeyChecking=no $AWS_INSTANCE "ps -A | grep rulebot | awk '{print \$1}' | xargs kill -2"
+ssh -i $SECRETS/aws-instance.pem -o StrictHostKeyChecking=no $AWS_INSTANCE "ps -A | grep rulebot | awk '{print \$1}' | xargs kill -2"
 
-scp -i $SECRETS/aws-rulebot.pem rulebot $AWS_INSTANCE:/home/ec2-user/rulebot
+scp -i $SECRETS/aws-instance.pem rulebot $AWS_INSTANCE:/home/ec2-user/rulebot/rulebot
 
-ssh -i $SECRETS/aws-rulebot.pem -o StrictHostKeyChecking=no $AWS_INSTANCE "nohup /home/ec2-user/rulebot \
+ssh -i $SECRETS/aws-instance.pem -o StrictHostKeyChecking=no $AWS_INSTANCE "nohup /home/ec2-user/rulebot/rulebot \
     -discord-token $DISCORD_TOKEN \
     -google-cse $GOOGLE_CSE_ID \
     -google-token $GOOGLE_TOKEN \
     -kellen $KELLEN_ID \
-    -rulebooks /home/ec2-user/rulebooks \
-    -cache /home/ec2-user/cache/cache.json \
-    -logfile /home/ec2-user/logs/rulebot.log 1>/home/ec2-user/nohup.log 2>/home/ec2-user/nohup.log &"
+    -rulebooks /home/ec2-user/rulebot/rulebooks \
+    -cache /home/ec2-user/rulebot/cache/cache.json \
+    -logfile /home/ec2-user/rulebot/logs/rulebot.log 1>/home/ec2-user/rulebot/logs/nohup.log 2>/home/ec2-user/rulebot/logs/nohup.log &"
 
 rm rulebot
